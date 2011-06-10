a0d5e7116ad4fa2b6fd09afe953f2b9a030f92cf
Geir-Tore Lindsve <lindsve@gmail.com>
2011-06-10 19:26:42 +0200

    modified to create summary notification instead

--- Statlight.Growl/StatlightGrowl.cs
+++ Statlight.Growl/StatlightGrowl.cs
@@ -2,31 +2,44 @@
 using Growl.Connector;
 using StatLight.Client.Harness.Events;
 using StatLight.Core.Events;
+using StatLight.Core.Events.Aggregation;
 
 namespace Statlight.Growl
 {
-    public class StatlightGrowl : ITestingReportEvents
+    public class StatlightGrowl : ITestingReportEvents, IListener<TestRunCompletedServerEvent>
     {
         private GrowlConnector _growl;
+        private int _noOfFailedTests = 0;
+        private int _noOfIgnoredTests = 0;
+        private int _noOfPassedTests = 0;
 
-        private const string appName = "Statlight";
-        private const string messageTestFailed = "FAILED";
+        private const string AppName = "Statlight";
+        private const string MessageTestsFailed = "FAILED";
+        private const string MessageTestsSucceeded = "SUCCEEDED";
 
         public StatlightGrowl()
         {
             _growl = new GrowlConnector();
 
             var application = new Application("Statlight");
-            var testComplete = new NotificationType("FAILED", "Test failed");
-            _growl.Register(application, new[] { testComplete});
+            var testCompleteWithFailedTests = new NotificationType(MessageTestsFailed, "Statlight");
+            var testCompleteWithOnlySuccessfulTests = new NotificationType(MessageTestsSucceeded, "Statlight");
+            _growl.Register(application, new[] { testCompleteWithFailedTests, testCompleteWithOnlySuccessfulTests});
         }
 
         public void Handle(TestCaseResult message)
         {
-            if (message.ResultType == ResultType.Failed)
+            switch (message.ResultType)
             {
-                var notification = new Notification(appName, messageTestFailed, null, message.ClassName, message.MethodName, null, true, Priority.Moderate, null);
-                _growl.Notify(notification);
+                case ResultType.Failed:
+                    _noOfFailedTests++;
+                    break;
+                case ResultType.Ignored:
+                    _noOfIgnoredTests++;
+                    break;
+                case ResultType.Passed:
+                    _noOfPassedTests++;
+                    break;
             }
         }
 
@@ -37,5 +50,19 @@ namespace Statlight.Growl
         public void Handle(FatalSilverlightExceptionServerEvent message) { }
 
         public void Handle(UnhandledExceptionClientEvent message) { }
+        
+        public void Handle(TestRunCompletedServerEvent message)
+        {
+            var notificationMessage = _noOfFailedTests > 0 ? MessageTestsFailed : MessageTestsSucceeded;
+
+            var summary = string.Format("Test run finished. {0} passed, {1} failed, {2} ignored", _noOfPassedTests, _noOfFailedTests, _noOfIgnoredTests);
+
+            var notification = new Notification(AppName, notificationMessage, null, AppName, summary, null, true, Priority.Moderate, null);
+            _growl.Notify(notification);
+
+            _noOfPassedTests = 0;
+            _noOfIgnoredTests = 0;
+            _noOfFailedTests = 0;
+        }
     }
 }
